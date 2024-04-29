4f181ae4c33b14dc98e93844575907a510d0b65d
Aswin Manohar <aswinmanohar@pop-os.fritz.box>
2024-04-30 00:16:29 +0200

    first commit personal site

---
+++ lib/writing.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import fs from "fs";
+import { getMdxContent, MaybeContent } from "./mdx";
+
+export interface Post {
+  title: string;
+  description: string;
+  image: string;
+  date: string;
+  url: string;
+  external: boolean;
+  source: string;
+}
+
+
+//* /Function getAllSlugs():
+/* Retrieves an array of slugs (URL components) for posts that are not marked as external:
+Calls getAllPostData() to get the full array of posts.
+Filters the posts using .filter(item => !item.external) to include only those posts that are not external.
+Maps over the filtered array to extract the url propert */
+
+export function getAllPostData(): Post[] {
+  return JSON.parse(
+    fs.readFileSync(
+      path.join(process.cwd(), "content", "writing", "external.json"),
+      "utf8"
+    )
+  );
+}
+
+export function getAllSlugs(): string[] {
+  const data = getAllPostData();
+
+  return data.filter((item) => !item.external).map((item) => item.url);
+}
+
+export async function getPost(slug: string): Promise<MaybeContent<Post>> {
+  return getMdxContent<Post>("writing", `${slug}.mdx`);
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,111 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from "../node_modules/next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Prose, withProse } from "@nikolovlazar/chakra-ui-prose";
+import Layout from "../components/Layout";
+import { ReactElement } from "react";
+import { DefaultSeo } from "next-seo";
+import posthog from "posthog-js";
+import React from "react";
+import { useRouter } from "../node_modules/next/router";
+import { Lora } from "@next/font/google";
+import { Image } from "@chakra-ui/react";
+
+
+const lora = Lora({ subsets: ["latin"], display: "swap" });
+
+/* const theme = extendTheme(
+  {
+    fonts: {
+      heading: lora.style.fontFamily,
+      body: lora.style.fontFamily,
+    },
+  },
+  withProse({
+    baseStyle: {
+      "h1, h2, h3, h4, h5, h6": {
+        mt: 4,
+        mb: 4,
+      },
+      p: {
+        my: 3,
+      },
+      a: {
+        color: "purple.500",
+      },
+    },
+  })
+);
+ */
+const theme = extendTheme(
+  {
+    fonts: {
+      heading: lora.style.fontFamily,
+      body: lora.style.fontFamily,
+    },
+  },
+  withProse({
+    baseStyle: {
+      'h1, h2, h3, h4, h5, h6': {
+        mt: 2,
+        mb: 2,
+        fontSize: '2xl' // Example to set font size for headings
+      },
+      p: {
+        my: 3,
+        fontSize: 'md' // Example to set font size for paragraphs
+      },
+      a: {
+        color: "blue.500",
+        fontSize: 'sm' // Example to set font size for links
+      },
+    },
+  })
+);
+
+
+
+const getDefaultLayout = (page: ReactElement) => (
+  <Layout>
+    <Prose>{page}</Prose>
+  </Layout>
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const getLayout = Component.getLayout || getDefaultLayout;
+
+  React.useEffect(() => {
+    posthog.init("phc_jFlJqpi333LZJJRxwjiFTkKI2Ufv3Pgf0hnbrPuZdLL", {
+      api_host: "https://app.posthog.com",
+    });
+
+    const handleRouteChange = () => posthog.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <DefaultSeo
+        title="Aswin Manohar"
+        description="I'm a constant learner and aspiring technical generalist. I'm also a founding enginer at thirdweb and on gap year from the University of Pennsylvania."
+        openGraph={{
+          title: "Aswin Manohar",
+          description:
+            "I'm a constant learner and aspiring technical generalist. I'm also a founding enginer at thirdweb and on gap year from the University of Pennsylvania.",
+          images: [
+            {
+              url: "https://adammaj.com/og-image-dark.jpg",
+              type: "image/jpeg",
+            },
+          ],
+          siteName: "Aswin Manohar",
+        }}
+      />
+      {getLayout(<Component {...pageProps} />)}
+    </ChakraProvider>
+  );
 }

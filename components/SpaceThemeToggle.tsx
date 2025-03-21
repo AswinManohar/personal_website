84a613e490f30dca95d3e0246bc565ddfa40bd75
AswinManohar <aswinbio@gmail.com>
2025-03-21 22:37:26 +0100

    themes and icons

--- components/SpaceThemeToggle.tsx
+++ components/SpaceThemeToggle.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import { IconButton, Tooltip, useColorModeValue, Box } from '@chakra-ui/react';
-import { FaRocket, FaRegCircle } from 'react-icons/fa';
+import { IconButton, Tooltip, useColorModeValue, Box, Image } from '@chakra-ui/react';
+import { FaRocket } from 'react-icons/fa';
 import { useTheme } from '../context/ThemeContext';
 
 const SpaceThemeToggle = () => {
   const { spaceThemeEnabled, toggleSpaceTheme } = useTheme();
   const iconColor = useColorModeValue('gray.600', 'gray.200');
-
+  
   return (
     <Box display="inline-block" ml={2} cursor="pointer">
-    <Tooltip label={spaceThemeEnabled ? 'Disable Space Theme' : 'Enable Space Theme'}>
-      <IconButton
-        aria-label="Toggle space theme"
-        icon={spaceThemeEnabled ? <FaRocket /> : <FaRegCircle />}
-        onClick={toggleSpaceTheme}
-        variant="ghost"
-        color={iconColor}
-        size="lg"
-        fontSize="24px"
-        p={2}
-      />
-    </Tooltip>
+      <Tooltip label={spaceThemeEnabled ? 'Disable Space Theme' : 'Enter void and get lost'}>
+        <IconButton
+          aria-label="Toggle space theme"
+          icon={
+            spaceThemeEnabled ? (
+              <FaRocket />
+            ) : (
+              <Image 
+                src="/HD-wallpaper-8-bit-moon-sky-space-modified.png" 
+                alt="Planet" 
+                width="24px" 
+                height="24px" 
+                objectFit="contain"
+              />
+            )
+          }
+          onClick={toggleSpaceTheme}
+          variant="unstyled"
+          color={iconColor}
+          size="lg"
+          fontSize="24px"
+          p={2}
+          _focus={{ boxShadow: 'none', outline: 'none' }}
+          _active={{ bg: 'transparent', transform: 'none' }}
+          _hover={{ bg: 'transparent' }}
+          style={{ outline: 'none' }}
+        />
+      </Tooltip>
     </Box>
   );
 };
